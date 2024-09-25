944c1e9c2e9bfef43ede5ff0940fd46af771ec15
Devante Young <devante.j.young.mil@army.mil>
2024-09-24 22:05:12 -0500

    lab1

---
+++ adv_web_programming_class/REACT/reactDemo1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Greetings from './components/Greetings';
+
+const App = () => {
+  return (
+    <div>
+      <Greetings  name="De'vante "/>
+    </div>
+  );
+};
+
+export default App;
