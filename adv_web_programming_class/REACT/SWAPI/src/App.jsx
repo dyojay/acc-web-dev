8bd860b0fdab7b4d2f5a401d242d9317f3dd8530
Devante Young <devante.j.young.mil@army.mil>
2024-10-06 17:34:17 -0500

    SWAPI LAB3

---
+++ adv_web_programming_class/REACT/SWAPI/src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useState } from 'react';
+import './App.css';
+import { Button, Stack } from '@mui/material';
+import Table from './components/Table';
+
+function App() {
+  const [showTable, setShowTable] = useState(false);
+  const [fetchTrigger, setFetchTrigger] = useState(0);
+  const handleSubmit = () => {
+    setShowTable(true);
+    setFetchTrigger(prev => prev + 1);
+  };
+  
+  const handleReset = () => {
+    setShowTable(false);
+    setFetchTrigger(0);
+  };
+
+
+  
+  return (
+    <div className='app-background'>
+      
+        <h1>SWAPI</h1>
+        <h2>Star Wars API</h2>
+       <div>
+       <Stack spacing={2} direction="row" justifyContent="center">
+        <Button variant="contained" color="primary" onClick={handleSubmit}>Submit</Button>
+        <Button variant="contained" color="warning" onClick={handleReset}>Reset</Button>
+      </Stack>
+    </div>
+    {showTable && <Table fetchTrigger={fetchTrigger} />}
+    </div>
+  );
+}
+
+export default App;
---
+++ adv_web_programming_class/REACT/SWAPI/src/components/Table.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Table.css'; 
+
+const Table = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://swapi.dev/api/people/');
+        setData(response.data.results);
+        setIsLoading(false);
+      } catch (error) {
+        setError('An error occurred while fetching data');
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
+
+  return (
+    <table className="star-wars-table">
+      <caption>Star Wars Characters</caption>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Height</th>
+          <th>Hair Color</th>
+          <th>Gender</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((character, index) => (
+          <tr key={index}>
+            <td>{character.name}</td>
+            <td>{character.height}</td>
+            <td>{character.hair_color}</td>
+            <td>{character.gender}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
