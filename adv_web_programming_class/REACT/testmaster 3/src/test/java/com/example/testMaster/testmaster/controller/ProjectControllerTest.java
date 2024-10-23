d45c6c19ac449ba2c2d0974967f513f5bb1dd922
Devante Young <devante.j.young.mil@army.mil>
2024-10-23 09:56:48 -0500

    frontend push

---
+++ adv_web_programming_class/REACT/testmaster 3/src/test/java/com/example/testMaster/testmaster/controller/ProjectControllerTest.java
@@ -0,0 +1,25 @@
+package com.example.testMaster.testmaster.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectControllerTest {
+
+    @Test
+    void getAllProjects() {
+
+    }
+
+    @Test
+    void createProject() {
+    }
+
+    @Test
+    void getProjectById() {
+    }
+
+    @Test
+    void deleteProject() {
+    }
+}
